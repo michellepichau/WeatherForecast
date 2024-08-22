ff164187c197de02b4ca7ea13ad8ff8660120255
michellepichau <50593955+michellepichau@users.noreply.github.com>
2024-08-21 23:26:13 -0300

    Add files via upload

---
+++ WeatherForecast/WeatherForecast/ContentView.swift
@@ -0,0 +1,68 @@
+//
+//  ContentView.swift
+//  WeatherForecast
+//
+//  Created by Michelle Pichau on 06/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        HStack {
+            DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
+            
+            DayForecast(day: "Tue", isRainy: true, high: 60, low: 40)
+
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+struct DayForecast: View {
+    let day: String
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+
+// icone
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        } else {
+            return "sun.max.fill"
+        }
+    }
+    
+// cor
+    var iconColor: Color {
+        if isRainy {
+            return Color.blue
+        } else {
+            return Color.yellow
+        }
+    }
+    
+    var body: some View {
+        
+        VStack {
+            Text(day)
+                .font(Font.headline)
+            Image(systemName: iconName)
+                .foregroundColor(iconColor)
+                .font(Font.largeTitle)
+                .padding(5)
+            Text("High: \(high)º")
+                .fontWeight(Font.Weight.semibold)
+                .padding(2)
+            Text("Low: \(low)º")
+                .fontWeight(Font.Weight.medium)
+                .foregroundStyle(Color.secondary)
+        }
+        .padding()
+    }
+}
---
+++ WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Michelle Pichau on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
